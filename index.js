50b1f879d1e89402c754c4778ca2b9434acdb58b
Hamad Bakeel <90466313+HamadBakeel@users.noreply.github.com>
2023-01-08 23:20:17 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 31, num2 = 2
+const multiply = num2 * num1;
+
+const random = Math.ceil(Math.random());
+
+const num3 = 16, num4 = 12;
+const mod = num3%num4;
+
+const set = [1,2,3,20];
+let max = set[0];
+for(let i=1 ; i<set.length ; i++)
+    max = set[i] > max ? set[i] : max;
